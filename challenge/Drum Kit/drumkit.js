4bc77040fa2d1ce2198c07838013cb030f8333fd
Kulasekaran <kulasekaranslrk@gmail.com>
2023-03-10 00:17:08 +0530

    corrected console error in drumkit site

--- challenge/Drum Kit/drumkit.js
+++ challenge/Drum Kit/drumkit.js
@@ -1,22 +1,25 @@
 var elemId = 0;
 document.addEventListener('click',function(event){
-  if(event.target.hasAttribute('id')){
+  if(event.target.id.slice(-3)==='btn'){
     event.target.children[2].currentTime = 0;
     event.target.children[2].play();
   }
-  else{
-    elemId = event.target.parentNode.getAttribute('id');
+  else if(event.target.parentNode.id.slice(-3)=='btn'){
     event.target.parentNode.children[2].currentTime = 0;
     event.target.parentNode.children[2].play();
   }
 });
+const buttons = document.querySelectorAll('button');
 document.addEventListener('keypress',function(event){
-  const buttons = document.querySelectorAll('button');
   buttons.forEach(button => {
     const secondChild = button.children[1];
     if(event.key === secondChild.textContent.toLowerCase()){
       button.children[2].currentTime=0;
       button.children[2].play();
+      button.classList.add('clicked');
+      setTimeout(function(){
+        button.classList.remove('clicked');
+      },100);
     }
   });
 });
